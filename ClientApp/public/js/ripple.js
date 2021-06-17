e0545fdbfb29955e6c8288f9649095cf369c7a98
Denzel Awuah <denzelbawuah@gmail.com>
2021-06-17 15:15:06 -0400

    Added fonts, imgs, and js

---
+++ ClientApp/public/js/ripple.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  
+    "use strict";
+    $('.slider').ripples({
+              resolution: 712,
+              dropRadius: 10, //px
+              perturbance: 5,
+          });
+  });
